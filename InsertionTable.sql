68afed26e8ce10edbb9ddad629ba5e6e9de7d245
Lenny.dn <lenny.de-nardi@etu.univ-orleans.fr>
2022-09-19 19:38:11 +0200

    Insertion + test de la table RESERVER

--- InsertionTable.sql
+++ InsertionTable.sql
@@ -48,11 +48,23 @@ insert into PERSONNE values(1, "Doudeau", "Luis", 2003-10-03, 70, "luis.doudeau@
 
 insert into PONEYS values(1, "Chacha", 180),
                             (2, "Michella", 30),
-                            (3, "Parapluie", 42),
+                            (3, "Parapluie", 41),
                             (4, "Merveille", 101),
                             (5, "Shakira", 100),
-                            (6, "Souja boy", 55),
+                            (6, "Soulja boy", 55),
                             (7, "Prune", 84),
                             (8, "Virgule", 40),
                             (9, "Bouteille", 62),
                             (10, "Mure", 71);
+
+insert into RESERVER values(2022-09-12 14:30, 1, 1, 1, 01:00:00, true),
+                            (2022-09-12 16:30, 2, 14, 2, 02:00:00, false), --pas possible car poidssup inférieur poids
+                            (2022-09-13 09:00, 3, 3, 4, 02:00:00, true),
+                            (2022-09-13 11:30, 4, 12, 4, 02:00:00, true), -- pas possible car poney doit reposé au moins 1h
+                            (2022-09-18 13:45, 5, 9, 7, 02:00:00, false),
+                            (2022-09-18 16:45, 6, 11, 7, 01:00:00, true), -- fonctionne si on compte qu'au bout de 1h pile le cheval refonctionne
+                            (2022-09-18 17:00, 7, 11, 1, 02:00:00, false),
+                            (2022-09-18 18:30, 8, 11, 5, 01:00:00, true), -- CETTE INSERTION + CELLE D'AVANT = 2 SUR LE MEME COURS A LA MM HEURE AVEC CHEVAUX DIFFÉRENTS DONC VOIR SI ON PEUT
+                            (2022-09-18 20:20, 9, 2, 3, 01:00:00, false), -- POIDS DU MEC = POIDSSUP CHEVAL DONC NORMALEMENT ÇA FONCTIONNE
+                            (2022-09-19 08:00, 2, 14, 1, 02:00:00, false), -- meme mec/cheval que l'insertion 1 sauf qu'il a pas encore payé
+                            (2022-09-19 09:30, 2, 10, 1, 01:00:00, true), -- pas possible car insertion d'avant le mec est entrain de faire du cheval deja
