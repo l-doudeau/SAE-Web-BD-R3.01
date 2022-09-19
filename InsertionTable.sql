6b1030762b61b37ee8eb69d4f2f7951ab997ec18
DOUDEAU Luis <o22103029@info27-19.iut45.univ-orleans.fr>
2022-09-19 16:56:28 +0200

    Insertion des données

---
+++ InsertionTable.sql
@@ -0,0 +1,38 @@
+insert into CLIENT values(1, true),
+                          (2, false),
+                          (3, false),
+                          (4, false),
+                          (5, true),
+                          (6, true),
+                          (7, true),
+                          (8, false),
+                          (9, false),
+                          (10, true),
+                          (11, false),
+                          (12, true);
+    
+insert into COURS values(1, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Collectif", 45), 
+                           (2, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Individuel", 80),
+                           (3, 'Initiation', "Cours d'initiation au poney ", "Individuel", 65),
+                           (4, 'Initiation', "Cours d'initiation au poney ", "Collectif", 25),
+                           (5, 'Cours de course', "Ce cours portera sur la course à poney", "Individuel", 65),
+                           (6, 'Cours poussin', "Ce cours sera un entraînement pour la catégorie poussin", "Collectif", 35),
+                           (7, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Collectif", 45),
+                           (8, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Individuel", 45),
+                           (9, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Individuel", 45),
+                           (10, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Collectif", 45),
+                           (11, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Individuel", 45),
+                           (12, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Collectif", 45),
+                           (13, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Collectif", 45),
+                           (14, 'Cours de saut', "Ce cours portera sur la technique de saut d'obstacle pour les débutants", "Individuel", 100);
+
+
+insert into MONITEUR values(1), 
+                          (12),
+                          (13),
+                          (14),
+                          (15),
+                          (16);
+
+
+insert into PERSONNE values(), --TODO
