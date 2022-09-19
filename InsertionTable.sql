819dfaf76b47b25b8f92506453499373ed947d5e
Lenny.dn <lenny.de-nardi@etu.univ-orleans.fr>
2022-09-19 19:05:23 +0200

    Insertion table personne

--- InsertionTable.sql
+++ InsertionTable.sql
@@ -35,4 +35,13 @@ insert into MONITEUR values(1),
                           (16);
 
 
-insert into PERSONNE values(), --TODO
+insert into PERSONNE values(1, "Doudeau", "Luis", 2003-10-03, 70, "luis.doudeau@gmail.com", "4 rue des chèvres", 45000, "Orléans", 0607080910, "chèvre123!"), 
+                            (2, "Faucher", "Thomas", 2003-07-06, 55, "thelendpvp@icloud.com", "10 chemin du tron", 45000, "Orléans", 0610141820, "TfaucherLMaO@a1"),
+                            (3, "De nardi", "Lenny", 2003-11-16, 55, "lenny.denardi@gmail.com", "49 rue de la biche", 45380, "Chaingy", 0708090506, "1!Y!o!Y!o!2"),
+                            (4, "Charpentier", "Maxym", 1975-06-01, 99, "Maxlamenace@gmail.com", "45 rue de la menace", 84550, "Mornas", 0710111213, "JvezvoustuarAHAH:"),
+                            (5, "Jory", "Jonathan", 2017-05-20, 35,"Jjory@icloud.com", "23 impasse de la groue", 70000, "Vesoul", 0790807010, "JORYGOLEPAS"),
+                            (6, "El mechta", "Aymen", 2000-15-12, 100, "mechta@gmail.com", "10 rue d'ivrogne", 40120, "Roquefort", 0615141921, "DO!RM/IR!"),
+                            (7, "Plu", "Dinspi", 2002-18-01, 150, "salut@gmail.com", "1 rue du lac", 12000, "Rodez", 0691919191, "INSPI123REV!"),
+                            (8, "Plu", "Claude", 1965-01-02, 85, "JCjc@icloud.com", "1 rue du lac", 12000, "Rodez", 0681818181, "JCAHJCAH"),
+                            (9, "Chirac", "Jacques", 1989-08-07, "Jesuisunfake@gmail.com", "84 rue du fake", 18260, "Barlieu", 0707080707, "fakercmoi!"),
+                            (10, "Retour", "Erve", 2010-10-10, "roides10@icloud.com", "10 rue des dix", 10000, "Troyes", 0710101010, "troyesmais10:p");
