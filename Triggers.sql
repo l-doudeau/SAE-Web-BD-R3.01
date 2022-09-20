1454e472ff1298fd6cf433dfd3cdd7d10929e48e
DE_NARDI Lenny <o22106780@info07-19.iut45.univ-orleans.fr>
2022-09-20 12:16:18 +0200

    Modification message SQLSTATE + insertion

--- Triggers.sql
+++ Triggers.sql
@@ -8,7 +8,7 @@ create or replace trigger verifPoids before insert on RESERVER for each row
         select poids into poidsPersonne from PERSONNE where idp = new.idp;
         select poidssup into poidsup from PONEYS where idpo = new.idpo;
         if poidsup < poidsPersonne then
-            set msg = concat(' Réservation impossible car le poids supporté par le poney est inférieur au poids de la personne');
+            set msg = concat(" Réservation impossible car le poids supporté par le poney d'id : ", new.idpo," est inférieur au poids de la personne d'id : ", new.idp);
             signal SQLSTATE '45000' set MESSAGE_TEXT = msg;
         end if;
     end |
