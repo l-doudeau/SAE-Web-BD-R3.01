e4964ca1ccb39374c743ebab47705314a37f96a8
DE_NARDI Lenny <o22106780@info12-24.iut45.univ-orleans.fr>
2022-11-15 14:12:09 +0100

    Ajout des tables ancien

--- scripts/script.sql
+++ scripts/script.sql
@@ -10,6 +10,12 @@ drop table if exists COURS;
 drop table if exists CLIENT;
 drop table if exists MONITEUR;
 drop table if exists PERSONNE;
+drop table if exists ANCIEN_RESERVER;
+drop table if exists ANCIEN_PONEYS;
+drop table if exists ANCIEN_COURS;
+drop table if exists ANCIEN_CLIENT;
+drop table if exists ANCIEN_MONITEUR;
+drop table if exists ANCIEN_PERSONNE;
 
 -- création des tables
 
@@ -72,6 +78,64 @@ CREATE TABLE RESERVER (
   PRIMARY KEY (jmahms, idp, idpo)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
+CREATE TABLE ANCIEN_PERSONNE (
+  idp int,
+  nomp VARCHAR(42),
+  prenomp VARCHAR(42),
+  ddn date,
+  poids decimal(3.3),
+  adressemail VARCHAR(42),
+  adresse VARCHAR(42),
+  code_postal int,
+  ville VARCHAR(20),
+  numerotel VARCHAR(20),
+  mdp VARCHAR(42),
+  PRIMARY KEY (idp)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE ANCIEN_MONITEUR (
+  idp int,
+  PRIMARY KEY (idp)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE ANCIEN_CLIENT (
+  idp int,
+  cotisationA boolean,
+  PRIMARY KEY (idp)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;  
+
+
+CREATE TABLE ANCIEN_COURS (
+  idc int,
+  nomc VARCHAR(42),
+  descc VARCHAR(300),
+  typec VARCHAR(42),
+  prix decimal(4.2),
+  PRIMARY KEY (idc)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE ANCIEN_PONEYS (
+  idpo int,
+  nomp VARCHAR(42),
+  poidssup decimal(3.3),
+  PRIMARY KEY (idpo)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+
+CREATE TABLE ANCIEN_RESERVER (
+
+  jmahms datetime,
+  idp int,
+  idc int,
+  idpo int,
+  duree time,
+  a_paye boolean,
+  PRIMARY KEY (jmahms, idp, idpo)
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
 -- les contraintes
 
 ALTER TABLE MONITEUR ADD FOREIGN KEY (idp) REFERENCES PERSONNE (idp);
@@ -79,6 +143,11 @@ ALTER TABLE CLIENT ADD FOREIGN KEY (idp) REFERENCES PERSONNE (idp);
 
 ALTER TABLE RESERVER ADD FOREIGN KEY (idpo) REFERENCES PONEYS (idpo);
 ALTER TABLE RESERVER ADD FOREIGN KEY (idc) REFERENCES COURS (idc);
+ALTER TABLE ANCIEN_MONITEUR ADD FOREIGN KEY (idp) REFERENCES ANCIEN_PERSONNE (idp);
+ALTER TABLE ANCIEN_CLIENT ADD FOREIGN KEY (idp) REFERENCES ANCIEN_PERSONNE (idp);
+
+ALTER TABLE ANCIEN_RESERVER ADD FOREIGN KEY (idpo) REFERENCES ANCIEN_PONEYS (idpo);
+ALTER TABLE ANCIEN_RESERVER ADD FOREIGN KEY (idc) REFERENCES ANCIEN_COURS (idc);
 
 -- ALTER TABLE ADD CONSTRAINT CHK_Poids CHECK ((select poids from PERSONNE where idp = RESERVER.idp) <= (select poidssup from PONEYS where idpo = PONEYS.idpo));
 -- peut-être mieux que le trigger 'verifPoids'
